94c961c7005ee83c28836c6c8068523ab985c403
MindWarpProd <99101069+MindWarpProd@users.noreply.github.com>
2022-11-14 08:30:48 -0800

    adding cool down

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 3.5f;
+    [SerializeField] GameObject _laser;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        CalculateMovement();
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laser, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), Quaternion.identity);
+        }
+
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
-        
+        Vector3 direction = new Vector3(horizontalInput, verticalInput);
+        transform.Translate(direction * speed * Time.deltaTime);
+        if (transform.position.y <= -4 || transform.position.y >= 0)
+        {
+            float positionY;
+            positionY = transform.position.y;
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(positionY, -5, 0));
+        }
+
+        if (transform.position.x <= -11)
+        {
+            transform.position = new Vector3(11, transform.position.y);
+        }
+        else if (transform.position.x >= 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y);
+        }
     }
 }
